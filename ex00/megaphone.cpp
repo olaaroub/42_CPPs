d38b530a514cf0e67953762bed5559e85708675d
olaaroub <olaaroub@e1r9p3.1337.ma>
2025-01-04 22:11:35 +0100

    ex00 done

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: olaaroub <olaaroub@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/04 16:40:41 by olaaroub          #+#    #+#             */
+/*   Updated: 2025/01/04 17:06:23 by olaaroub         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int ac, char **av)
+{
+    if (ac < 2)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return (0);
+    }
+    while(*++av)
+    {
+        std::string str = *av;
+        for (size_t i = 0; i < str.length(); i++)
+            std::cout << (char)toupper(str[i]) << std::flush;
+    }
+    std::cout << std::endl;
+}
