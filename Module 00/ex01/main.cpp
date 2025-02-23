7f4bd4cefeea488164848b32995729fae0b11600
olaaroub <olaaroub@e1r8p8.1337.ma>
2025-02-23 22:08:32 +0000

    reverted to last version ater session reset

--- Module 00/ex01/main.cpp
+++ Module 00/ex01/main.cpp
@@ -6,14 +6,14 @@
 /*   By: olaaroub <olaaroub@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 18:22:52 by olaaroub          #+#    #+#             */
-/*   Updated: 2025/02/23 18:58:18 by olaaroub         ###   ########.fr       */
+/*   Updated: 2025/02/22 21:44:25 by olaaroub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "contact.hpp"
 #include "phonebook.hpp"
 
-static void addContact(PhoneBook *book)
+static void add_contact(PhoneBook *book)
 {
     Contact contact;
     std::string first_name;
@@ -24,78 +24,48 @@ static void addContact(PhoneBook *book)
 
     std::cout << "First name: ";
     std::getline(std::cin, first_name);
-    if (std::cin.eof())
+    if(std::cin.eof())
         return;
     std::cout << "Last name: ";
     std::getline(std::cin, last_name);
-    if (std::cin.eof())
+    if(std::cin.eof())
         return;
     std::cout << "nickname: ";
     std::getline(std::cin, nickname);
-    if (std::cin.eof())
+    if(std::cin.eof())
         return;
     std::cout << "Phone number: ";
     std::getline(std::cin, phone_number);
-    if (std::cin.eof())
+    if(std::cin.eof())
         return;
     std::cout << "Darkest secret: ";
     std::getline(std::cin, darkest_secret);
-    if (std::cin.eof())
-        return;
+    if(std::cin.eof())
+    return;
     std::cout << std::endl;
     contact.set_first_name(first_name);
     contact.set_last_name(last_name);
     contact.set_nickname(nickname);
     contact.set_phone_number(phone_number);
     contact.set_darkest_secret(darkest_secret);
-    book->addContact(contact);
-}
-
-bool isNonPrint(std::string line){
-    for (size_t i = 0; i < line.length(); i++){
-        if (!isprint(line[i]))
-            return true;
-    }
-    return false;
-}
-
-bool isWhiteSpaces(std::string line)
-{
-    for (size_t i = 0; i < line.length(); i++)
-    {
-        if (!isspace(line[i]))
-            return false;
-    }
-    return true;
-}
-
-void getInput(std::string line)
-{
-    std::string test;
-    while (isNonPrint(test) || isWhiteSpaces(line))
-    {
-        std::cout << "->> ";
-        std::getline(std::cin, test);
-        if (std::cin.eof())
-            exit(0);
-    }
-    line = test;
+    book->add_contact(contact);
 }
 
 int main()
 {
     PhoneBook book;
-    std::string line;
+    std::string pr;
 
     while (1)
     {
-        getInput(line);
-        if (line == "EXIT")
+        std::cout << "$> ";
+        std::getline(std::cin, pr);
+        if (pr == "EXIT" || std::cin.eof())
             break;
-        else if (line == "ADD")
-            addContact(&book);
-        else if (line == "SEARCH")
-            book.searchContact();
+        else if (pr == "ADD")
+           add_contact(&book);
+        else if( pr == "SEARCH")
+            book.search_contact();
     }
     return 0;
 }
---
+++ Module 00/ex01/phonebook.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: olaaroub <olaaroub@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/22 14:37:59 by olaaroub          #+#    #+#             */
+/*   Updated: 2025/02/22 21:27:03 by olaaroub         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+#define PHONEBOOK_HPP
+
+#include "contact.hpp"
+
+class PhoneBook
+{
+	Contact _contacts[3];
+	int _i;
+
+public:
+	void add_contact(Contact contact);
+	void    search_contact();
+	PhoneBook(){_i = 0;}
+};
+
+#endif
