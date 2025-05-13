0e6bcf1bbb56516b4668fa810cc9b79ef8157f2d
olaaroub <olaaroub@e1r9p12.1337.ma>
2025-05-13 16:57:11 +0100

    prog

---
+++ Module 02/ex02/Fixed.hpp
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: olaaroub <olaaroub@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/24 21:51:17 by olaaroub          #+#    #+#             */
+/*   Updated: 2025/05/04 20:34:17 by olaaroub         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed{
+
+private:
+    int                 _value;
+    static const int    _fractionalBits = 8;
+
+public:
+    Fixed();
+    ~Fixed();
+
+    Fixed(const int intValue);
+    Fixed(const float floatValue);
+    Fixed(const Fixed &obj);
+
+    int getRawBits(void) const;
+    void setRawBits(int const raw);
+    float   toFloat(void) const;
+    int     toInt(void) const;
+
+    Fixed& operator=(const Fixed &obj);
+
+    bool operator>(const Fixed &obj) const;
+    bool operator<(const Fixed &obj) const;
+    bool operator<=(const Fixed &obj) const;
+    bool operator>=(const Fixed &obj) const;
+    bool operator==(const Fixed &obj) const;
+    bool operator!=(const Fixed &obj) const;
+
+    Fixed operator+(const Fixed &obj) const;
+    Fixed operator-(const Fixed &obj) const;
+    Fixed operator*(const Fixed &obj) const;
+    Fixed operator/(const Fixed &obj) const;
+
+    Fixed& operator++(); // Pre-increment
+    Fixed operator++(int); // Post-increment
+    Fixed& operator--(); // Pre-decrement
+    Fixed operator--(int); // Post-decrement
+
+
+};
+
+std::ostream& operator<<(std::ostream &ostream, Fixed const &o);
+
+#endif
