c28aa90e3e4ef31621d07a97cdc83bf1eab94eea
Oussama Laaroubi <oussamalaarroubi@gmail.com>
2025-05-26 20:24:59 +0100

    removed unessecary

--- 42Cursus/Rank_06/ft_transcendence/friend/users/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import FriendsView
-
-urlpatterns = [
-    path('friends/', FriendsView.as_view(), name='friends'),
-]
--- 42Cursus/Rank_06/ft_transcendence/matchmaking/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('tournement/register/', views.RegistreTournement.as_view(), name='start_tournement'),
-    path('tournement/finish/', views.FinishTournement.as_view(), name='finish_tournement'),
-    path('game/register/', views.RegistreGame.as_view(), name='start_game'),
-    path('game/finish/', views.FinishGame.as_view(), name='finish_game'),
-]
