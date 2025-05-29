7f2c217249eeab2e4a96415f41459dc91ac98807
olaaroub <olaaroub@e1r4p1.1337.ma>
2025-05-29 22:52:17 +0100

    pushed 01

--- Module 02/ex01/main.cpp
+++ Module 02/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: olaaroub <olaaroub@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 12:24:48 by olaaroub          #+#    #+#             */
-/*   Updated: 2025/05/03 12:25:42 by olaaroub         ###   ########.fr       */
+/*   Updated: 2025/05/29 22:06:53 by olaaroub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,14 @@ int main()
     Fixed const b( 10 );
     Fixed const c( 42.42f );
     Fixed const d( b );
+
     a = Fixed( 1234.4321f );
+		
     std::cout << "a is " << a << std::endl;
     std::cout << "b is " << b << std::endl;
     std::cout << "c is " << c << std::endl;
     std::cout << "d is " << d << std::endl;
+
     std::cout << "a is " << a.toInt() << " as integer" << std::endl;
     std::cout << "b is " << b.toInt() << " as integer" << std::endl;
     std::cout << "c is " << c.toInt() << " as integer" << std::endl;
